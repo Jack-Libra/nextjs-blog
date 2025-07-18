618817c496759176bfe0fa754ca7586d30650095
Jack-Libra <dafsf60804@gmail.com>
2025-07-19 01:01:31 +0800

    測試自動部署

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true, // GitHub Pages 不支援 Next.js 圖片最佳化
+  },
+  basePath: '/nextjs-blog',      // 這裡必須和 GitHub repo 名稱相同
+  assetPrefix: '/nextjs-blog/',  // 靜態資源路徑前綴
 };
 
 export default nextConfig;
